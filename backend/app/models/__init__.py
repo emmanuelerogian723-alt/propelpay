082fbb4c7e1e7f499f9ad7c4ff4b3b6e32482ca6
emmanuelerogian723-alt <emmanuelerogian723@gmail.com>
2026-06-29 16:16:59 +0100

    feat: register EmailLog + BankAccount models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -6,4 +6,5 @@ from .payment import Payment
 from .signature import Signature
 from .follow_up import FollowUp
 from .subscription import Subscription
-__all__ = ["User","Client","Proposal","Invoice","InvoiceItem","Payment","Signature","FollowUp","Subscription"]
+from .email_log import EmailLog
+from .bank_account import BankAccount
