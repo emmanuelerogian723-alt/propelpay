9a6354d4c9531f24205936700cf08ae9cdcedda9
emmanuelerogian723-alt <emmanuelerogian723@gmail.com>
2026-06-30 20:19:01 +0100

    feat: register Project + ProjectUpdate models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,11 +1,18 @@
-from .user import User
-from .client import Client
-from .proposal import Proposal
-from .invoice import Invoice, InvoiceItem
-from .payment import Payment
-from .signature import Signature
-from .follow_up import FollowUp
-from .subscription import Subscription
-from .email_log import EmailLog
-from .bank_account import BankAccount
-from .recurring_invoice import RecurringInvoice
+from app.models.user import User
+from app.models.client import Client
+from app.models.proposal import Proposal
+from app.models.invoice import Invoice, InvoiceItem
+from app.models.payment import Payment
+from app.models.follow_up import FollowUp
+from app.models.signature import Signature
+from app.models.subscription import Subscription
+from app.models.recurring_invoice import RecurringInvoice
+from app.models.email_log import EmailLog
+from app.models.bank_account import BankAccount
+from app.models.project import Project, ProjectUpdate
+
+__all__ = [
+    "User","Client","Proposal","Invoice","InvoiceItem","Payment",
+    "FollowUp","Signature","Subscription","RecurringInvoice",
+    "EmailLog","BankAccount","Project","ProjectUpdate"
+]
